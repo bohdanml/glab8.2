fc5322b5cd58d7924abdba43c65ae3a3fb2ae74b
bohdanml <146237023+bohdanml@users.noreply.github.com>
2023-11-22 18:13:53 +0200

    Update lab.cpp

--- glab8.2/lab.cpp
+++ glab8.2/lab.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+//перевірка
 bool Limit(char ch, int start, int end) {
     return (int(ch) >= start && int(ch) <= end);
 }
-
+// трансформація  до першого пробілу
 string transformString(string inputString) {
     string result;
+    // знаход перший пробілу 
     size_t IndexOFfirstSpace = inputString.find_first_of(' ');
-
+    //проходжусь по символ до 1 пробілу
     for (int i = 0; i < IndexOFfirstSpace; i++) {
         char currentChar = inputString[i];
 
         if (Limit(currentChar, 97, 122) || Limit(currentChar, 48, 57)) {
-            // Convert lowercase letters and numbers to uppercase
+            //  малі літери на великі
             result += char(int(currentChar) - 32);
         }
         else {
+            //інші без змін
             result += currentChar;
         }
     }
-
-    result += inputString.substr(IndexOFfirstSpace); // Append the rest of the string
+    //решта без змін додаю
+    result += inputString.substr(IndexOFfirstSpace); 
     return result;
 }
 
@@ -30,9 +32,9 @@ int main() {
     string inputStr;
     cout << " Enter string: " << endl;
     getline(cin, inputStr);
-
+    //знахід пробілу
     if (inputStr.find_first_of(' ') != string::npos) {
-        string outputStr = transformString(inputStr);
+        string outputStr = transformString(inputStr);//якщо є пробіл трансформ
         cout << "  String after changes: " << endl << outputStr << endl;
     }
     else {
